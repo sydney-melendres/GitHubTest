90d11a1eb69ad0777eb69e1cee4608b800058d80
Sydney Melendres <sydneymelendres@gmail.com>
2023-02-15 09:50:30 +1100

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Sydney Melendres on 15/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
